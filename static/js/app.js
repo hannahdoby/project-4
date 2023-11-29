6a59034a0c8c0ed2c0bd67cd5979650a0de182a7
Andrew-Bourgeois <experiment626@macbook-pro.lan>
2023-11-28 19:41:43 -0500

    Added console logs for Rating variables and removed ratings clearing statement.

--- static/js/app.js
+++ static/js/app.js
@@ -5,23 +5,41 @@ document.getElementById('movieRatingForm').addEventListener('submit', function (
 
     // Collect ratings for each genre
     const actionRating = document.querySelector('input[name="actionRating"]:checked') ? document.querySelector('input[name="actionRating"]:checked').value : 0;
+    console.log('Action: ${actionRating}')
     const adventureRating = document.querySelector('input[name="adventureRating"]:checked') ? document.querySelector('input[name="adventureRating"]:checked').value : 0;
+    console.log('Adventure: ${adventureRating}')
     const animationRating = document.querySelector('input[name="animationRating"]:checked') ? document.querySelector('input[name="animationRating"]:checked').value : 0;
+    console.log('Animation: ${animationRating}')
     const childrenRating = document.querySelector('input[name="childrenRating"]:checked') ? document.querySelector('input[name="childrenRating"]:checked').value : 0;
-    const comedyRating = document.querySelector('input[name="comedyRating"]:checked') ? document.querySelector('input[name="comedyRating"]:checked').value : 0;      
-    const crimeRating = document.querySelector('input[name="crimeRating"]:checked') ? document.querySelector('input[name="crimeRating"]:checked').value : 0;          
+    console.log('Children: ${childrenRating}')
+    const comedyRating = document.querySelector('input[name="comedyRating"]:checked') ? document.querySelector('input[name="comedyRating"]:checked').value : 0;
+    console.log('Comedy: ${comedyRating}')
+    const crimeRating = document.querySelector('input[name="crimeRating"]:checked') ? document.querySelector('input[name="crimeRating"]:checked').value : 0;
+    console.log('Crime: ${crimeRating}')
     const documentaryRating = document.querySelector('input[name="documentaryRating"]:checked') ? document.querySelector('input[name="documentaryRating"]:checked').value : 0;
+    console.log('Documentary: ${documentaryRating}')
     const dramaRating = document.querySelector('input[name="dramaRating"]:checked') ? document.querySelector('input[name="dramaRating"]:checked').value : 0;
-    const fantasyRating = document.querySelector('input[name="fantasyRating"]:checked') ? document.querySelector('input[name="fantasyRating"]:checked').value : 0;        
-    const filmnoirRating = document.querySelector('input[name="filmnoirRating"]:checked') ? document.querySelector('input[name="filmnoirRating"]:checked').value : 0;    
-    const horrorRating = document.querySelector('input[name="horrorRating"]:checked') ? document.querySelector('input[name="horrorRating"]:checked').value : 0;    
-    const musicalRating = document.querySelector('input[name="musicalRating"]:checked') ? document.querySelector('input[name="musicalRating"]:checked').value : 0;         
+    console.log('Drama: ${dramaRating}')
+    const fantasyRating = document.querySelector('input[name="fantasyRating"]:checked') ? document.querySelector('input[name="fantasyRating"]:checked').value : 0;
+    console.log('Fantasy: ${fantasyRating}')
+    const filmnoirRating = document.querySelector('input[name="filmnoirRating"]:checked') ? document.querySelector('input[name="filmnoirRating"]:checked').value : 0;
+    console.log('Film-Noir: ${filmnoirRating}')
+    const horrorRating = document.querySelector('input[name="horrorRating"]:checked') ? document.querySelector('input[name="horrorRating"]:checked').value : 0;
+    console.log('Horror: ${horrorRating}')
+    const musicalRating = document.querySelector('input[name="musicalRating"]:checked') ? document.querySelector('input[name="musicalRating"]:checked').value : 0;
+    console.log('Musical: ${musicalRating}')
     const mysteryRating = document.querySelector('input[name="mysteryRating"]:checked') ? document.querySelector('input[name="mysteryRating"]:checked').value : 0;
+    console.log('Mystery: ${mysteryRating}')
     const romanceRating = document.querySelector('input[name="romanceRating"]:checked') ? document.querySelector('input[name="romanceRating"]:checked').value : 0;
+    console.log('Romance: ${romanceRating}')
     const scifiRating = document.querySelector('input[name="scifiRating"]:checked') ? document.querySelector('input[name="scifiRating"]:checked').value : 0;
+    console.log('Sci-Fi: ${scifiRating}')
     const thrillerRating = document.querySelector('input[name="thrillerRating"]:checked') ? document.querySelector('input[name="thrillerRating"]:checked').value : 0;
-    const warRating = document.querySelector('input[name="warRating"]:checked') ? document.querySelector('input[name=""]:checked').value : 0;        
+    console.log('Thriller: ${thrillerRating}')
+    const warRating = document.querySelector('input[name="warRating"]:checked') ? document.querySelector('input[name="warRating"]:checked').value : 0;
+    console.log('War: ${warRating}')
     const westernRating = document.querySelector('input[name="westernRating"]:checked') ? document.querySelector('input[name="westernRating"]:checked').value : 0;
+    console.log('Western: ${westernRating}')
 
     // Fetch new movie title
     const newMovieTitle = document.getElementById('newMovieTitle').value;
@@ -32,8 +50,7 @@ document.getElementById('movieRatingForm').addEventListener('submit', function (
 
 
     // Clear form fields after submission
-    document.getElementById('movieGenre').selectedIndex = 0;
-    document.getElementById('movieRating').value = '';
+    document.getElementById('newMovieTitle').value = '';
 });
 
 function predict(actionRating, adventureRating, animationRating, childrenRating, comedyRating, crimeRating, documentaryRating, dramaRating, fantasyRating, horrorRating, musicalRating, mysteryRating, romanceRating, scifiRating, thrillerRating, warRating, westernRating, newMovieTitle) {
